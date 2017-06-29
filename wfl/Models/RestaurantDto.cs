d3a3d1a1b8849ef0bc3321c6d89a6683a00a2e47
tnagaraja <tnagaraja@symphono.com>
2017-06-29 12:18:44 -0400

    Added MenuLink to RestaurantDto and Restaurant

--- wfl/Models/RestaurantDto.cs
+++ wfl/Models/RestaurantDto.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System;
+
 namespace Symphono.Wfl.Models
 {
     public class RestaurantDto
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public Uri MenuLink { get; set; }
     }
 }
