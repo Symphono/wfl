86e3894c07bd861af314d3d3c749586a4ba3db34
tnagaraja <tnagaraja@symphono.com>
2017-07-07 13:00:50 -0400

    Fixed Owin Configuration to be called on start up

--- wfl/DIContainerConfig.cs
+++ wfl/DIContainerConfig.cs
@@ -6,20 +6,9 @@ namespace Symphono.Wfl
 {
     public class DIContainerConfig
     {
-        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() => {
-            var container = new UnityContainer();
-            RegisterElements(container);
-            return container;
-        });
-
         public static void RegisterElements(IUnityContainer container)
         {
             container.RegisterType<IDBManager, MongoDBManager>();
-        }
-
-        public static IUnityContainer GetConfiguredContainer()
-        {
-            return container.Value;
         }
     }
 }
--- wfl/Global.asax.cs
+++ wfl/Global.asax.cs
@@ -4,9 +4,6 @@ namespace Symphono.Wfl
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
+       
     }
 }
