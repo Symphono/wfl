333c3c5589276ebb3da6fe919067768c275dd9db
tnagaraja <tnagaraja@symphono.com>
2017-07-11 13:38:08 -0400

    Added RestaurantProfile

---
+++ wfl/Profiles/RestaurantProfile.cs
@@ -0,0 +1,39 @@
+﻿using Hypermedia.Affordances;
+using Hypermedia.Transforms;
+using Hypermedia;
+using Symphono.Wfl.Controllers;
+using Symphono.Wfl.Models;
+using System.Net.Http;
+
+namespace Symphono.Wfl.Profiles
+{
+    public class RestaurantProfile: EntityProfile<Restaurant>
+    {
+        protected override void Configure(EntityConfiguration<Restaurant> configuration)
+        {
+            configuration
+                .UseRepresentationTransform(representation => representation
+                    .WithRepresentation("restaurant")
+                )
+                .UsePropertiesTransform(properties => properties
+                    .WithProperty(r => r.Name)
+                    .WithProperty(r => r.Id)
+                )
+                .UseLinkTransform(links => links
+                    .WithLink(l => l
+                        .WithRelation("self")
+                        .WithRepresentation("restaurant")
+                        .WithLink<Restaurant, RestaurantsController>(
+                            r => rc => rc.GetByIdAsync(r.Id)
+                        )
+                    )
+                 )
+                .UseLinkTransform(links => links
+                    .WithLink(l => l
+                        .WithRelation("menu")
+                        .WithTargetGenerator((r, h) => r.MenuLink)
+                    )
+               );
+        }
+    }
+}
