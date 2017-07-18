7216be2bb8d5ac0eb2e36a961f6dfdac51d4d470
tnagaraja <tnagaraja@symphono.com>
2017-07-18 13:13:42 -0400

    Added MenuSelectionProfile

---
+++ wfl/Profiles/MenuSelectionProfile.cs
@@ -0,0 +1,42 @@
+﻿using Hypermedia.Transforms;
+using Hypermedia;
+using Symphono.Wfl.Controllers;
+using Symphono.Wfl.Models;
+
+namespace Symphono.Wfl.Profiles
+{
+    public class MenuSelectionProfile: EntityProfile<MenuSelection>
+    {
+        protected override void Configure(EntityConfiguration<MenuSelection> configuration)
+        {
+            configuration
+                .UseRepresentationTransform(representation => representation
+                    .WithRepresentation("menu-selection")
+                )
+                .UsePropertiesTransform(properties => properties
+                    .WithProperty(s => s.Id)
+                    .WithProperty(s => s.FoodOrderId)
+                    .WithProperty(s => s.Description)
+                    .WithProperty(s => s.OrdererName)
+                )
+                .UseLinkTransform(links => links
+                    .WithLink(l => l
+                        .WithRelation("self")
+                        .WithRepresentation("menu-selection")
+                        .WithLink<MenuSelection, MenuSelectionsController>(
+                            s => mc => mc.GetByIdAsync(s.Id)
+                        )
+                    )
+                 )
+                .UseLinkTransform(links => links
+                    .WithLink(l => l
+                        .WithRelation("food-order")
+                        .WithRepresentation("food-order")
+                        .WithLink<MenuSelection, FoodOrdersController>(
+                            s => fc => fc.GetByIdAsync(s.FoodOrderId)
+                        )
+                    )
+                 );
+        }
+    }
+}
