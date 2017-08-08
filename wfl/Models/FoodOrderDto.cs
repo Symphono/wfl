d48ba136308b1179b6d3ac216cb8a1a5f26e57b8
tnagaraja <tnagaraja@symphono.com>
2017-08-08 15:34:59 -0400

    Removed validation check from class

--- wfl/Models/FoodOrderDto.cs
+++ wfl/Models/FoodOrderDto.cs
@@ -6,25 +6,5 @@ namespace Symphono.Wfl.Models
     public class FoodOrderDto
     {
         public string RestaurantId { get; set; }
-
-        public async Task<bool> CanCreateFoodOrderAsync(IDBManager<Restaurant> restaurantDBManager)
-        {
-            if (string.IsNullOrEmpty(RestaurantId) || ! await IsRestaurantIdValidAsync(restaurantDBManager))
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private async Task<bool> IsRestaurantIdValidAsync(IDBManager<Restaurant> restaurantDBManager)
-        {
-            Restaurant r = await restaurantDBManager.GetEntityByIdAsync(RestaurantId);
-            if (r != null)
-            {
-                System.Diagnostics.Debug.WriteLine(r.Name);
-                return true;
-            }
-            return false;
-        }
     }
 }
