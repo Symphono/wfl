a227696a9d91d55a6129c93ef6ac2539b365abc6
tnagaraja <tnagaraja@symphono.com>
2017-08-22 15:36:28 -0400

    Added functionality to create food order and see list of restaurants when the channel does not have an active order

--- wfl-slack-app/src/request-api.js
+++ wfl-slack-app/src/request-api.js
@@ -1,10 +1,68 @@
 var request = require('request');
 
+function getRestaurantIdByName(name, callback) {
+    let options = {
+        url: process.env.DB_URI + '/restaurant',
+        form: {
+            Name: name
+        }
+    }
+
+    request.get(options, function(err, res, body) {
+        let json = JSON.parse(body);
+        if(json.entities[0]) {
+            callback(json.entities[0].properties.Id);
+        }
+    });
+}
+
+function postFoodOrder(restaurantId, callback) {
+    let options = {
+        url: process.env.DB_URI + '/food-order',
+        form: {
+            RestaurantId: restaurantId
+        }
+    }
+
+    request.post(options, function(err, res, body) {
+        let json = JSON.parse(body);
+        if (json) {
+            callback(json.properties.Id);
+        }
+    });
+}
+
+function getRestaurants(callback)
+{
+    let options = {
+        url: process.env.DB_URI + '/restaurant'
+    }
+
+    request.get(options, function(err, res, body) {
+        let json = JSON.parse(body);
+        callback(json);
+    });
+}
+
 module.exports = {
-    logRestaurants: function(id) {
-        request(process.env.DB_URI + '/restaurant', function(err, res, body) {
-            let json = JSON.parse(body);
-            console.log(json.entities);
+    createFoodOrderFromRestaurantName: function(name, callback) {
+        getRestaurantIdByName(name, function(id) {
+            postFoodOrder(id, function(id) {
+                callback(id);
+            })
+        })
+    },
+    getArrayOfRestaurantNames: function(callback) {
+        getRestaurants(function(json) {
+            var names = [];
+            if (json.entities)
+            {
+                for (let restaurant of json.entities)
+                {
+                    names.push(restaurant.properties.Name);
+                }
+                callback(names);
+            }
         });
     }
 };
--- wfl-slack-app/src/wfl-bot.js
+++ wfl-slack-app/src/wfl-bot.js
@@ -2,28 +2,48 @@ var Slackbot = require('slackbots');
 var request = require('request');
 var requestApi = require('./request-api')
 
+var orderIdTable = {};
+
 var bot = new Slackbot({
     token: process.env.SLACK_BOT_TOKEN,
     name: "WFL bot"
 });
-
-bot.on('start', function() {
-    var params = {
-        icon_emoji: ':wave:'
-    };
-    bot.postMessageToUser('tnagaraja', 'wfl started up', params)
-})
 
 bot.on('message', function(data) {
     if (data.text)
     {
         if (data.text.indexOf('wfl order create') != -1)
         {
-            //create food order if none already exists
+            if(orderIdTable[data.channel])
+            {
+                bot.postMessage(data.channel, 'Sorry, there is already an active order for this channel. Please discard or complete the current order before creating a new one.');
+            }
+            else
+            {
+                var restaurantName = data.text.substring(data.text.indexOf('wfl order create') + 17);
+                requestApi.createFoodOrderFromRestaurantName(restaurantName, function(id) {
+                    orderIdTable[data.channel] = id;
+                    bot.postMessage(data.channel, 'OK, your food order has been created. You may now add menu selections!');
+                });
+            }
         }
-        else if (data.text == 'log restaurants')
+        else if (data.text.indexOf('wfl') != -1)
         {
-            requestApi.logRestaurants();
+            if (orderIdTable[data.channel])
+            {
+                bot.postMessage(data.channel, 'There is currently an active order for this channel.');
+            }
+            else
+            {
+                requestApi.getArrayOfRestaurantNames(function(names) {
+                    var message = 'This channel does not have an active order. Here is a list of restaurants you may want to choose from!\n';
+                    for (let name of names)
+                    {
+                        message = message + name + '\n';
+                    }
+                    bot.postMessage(data.channel, message);
+                })
+            }
         }
     }
 });
